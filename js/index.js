913a7e5f352088e1e15cd06974f2423bf9fb672f
hi363138911 <liangya363138911@126.com>
2017-10-13 18:23:45 +0800

    ES……

--- js/index.js
+++ js/index.js
@@ -1,36 +1,32 @@
 //tag
-var tag = location.search.split('?')[1] || '';
+let tag = location.search.split('?')[1] || '';
 if (decodeURI(tag) == '首页') {
     tag = '';
 }
 
 // 开始查询
-query()
-
-function query() {
-    var query = new AV.Query('Atricle');
+(function () {
+    const query = new AV.Query('Atricle');
     query.limit(1000);
     query.descending('createdAt');
     query.notEqualTo('hidden', 1); //hidden 不为 1 ，也就是不隐藏的。
     query.contains('tag', decodeURI(tag)); //注意转码
     query.find().then(function(results) {
-        var html = '';
-        for (var i = 0; i < results.length; i++) {
-            var id = results[i].id;
-            var title = results[i].get('title');
-            if (title.length > 70) title = title.substring(0, 70) + '......';
-            var content = results[i].get('content').substring(0, 100) + '......';
-            var time = results[i].createdAt.toLocaleString();
+        let html = '';
+        for (let i = 0; i < results.length; i++) {
+            const id = results[i].id;
+            const title = results[i].get('title').substring(0, 70) + '......';
+            const content = results[i].get('content').substring(0, 100) + '......';
+            const time = results[i].createdAt.toLocaleString();
             html += atricleHTML(id, title, content, time);
         }
         document.getElementById('content').innerHTML = html;
     }, function(error) {
         console.error(error);
     });
-}
+})();
 
 function atricleHTML(id, title, content, time) {
-
     return '<a class="item" href="atricle.html?' + id + '">' +
         '<h1 class="title">' + title + '</h1>' +
         '<div class="atricle-content">' + content + '</div>' +
@@ -46,14 +42,12 @@ document.getElementById('title').addEventListener('click', function() {
 }, false);
 
 
-
-var tagHTML = '标签：'
-var tagArr = tagStr.split(',')
-for (var i = 0; i < tagArr.length; i++) {
+let tagHTML = '标签：'
+const tagArr = tagStr.split(',')
+for (let i = 0; i < tagArr.length; i++) {
     tagHTML += ' <a href="index.html?' + tagArr[i] + '">' + tagArr[i] + '</a>';
 }
 document.getElementById('tag').innerHTML = tagHTML;
-
 
 //标签按钮
 document.getElementById('btn').onclick = function() {
@@ -63,7 +57,5 @@ document.getElementById('btn').onclick = function() {
     } else {
         this.innerText = "标签";
         document.getElementById('tag').style.visibility = 'hidden';
-
     }
-
 }
