8ed3e7d5caf5b2fbce4c4bb97118f26cfbf484e3
hi363138911 <liangya363138911@126.com>
2017-11-29 08:32:35 +0800

    还原网页本质.修改了一些CSS

--- js/index.js
+++ js/index.js
@@ -17,10 +17,8 @@ if (decodeURI(tag) == '首页') {
             const id = results[i].id
             let title = results[i].get('title')
             title = title.length < 70 ? title : title.substring(0, 70) + '......' 
-            let content = results[i].get('content')
-            content = content.length < 100 ? content : content.substring(0, 100) + '......' 
             const time = results[i].createdAt.toLocaleString()
-            html += atricleHTML(id, title, content, time)
+            html += atricleHTML(id, title, time)
         }
         document.getElementById('content').innerHTML = html
     }, function(error) {
@@ -28,10 +26,9 @@ if (decodeURI(tag) == '首页') {
     })
 })()
 
-function atricleHTML(id, title, content, time) {
+function atricleHTML(id, title, time) {
     return '<a class="item" href="atricle.html?' + id + '">' +
         '<h1 class="title">' + title + '</h1>' +
-        '<div class="atricle-content">' + content + '</div>' +
         '<div class="time">' + time + '</div></a>'
 }
 
