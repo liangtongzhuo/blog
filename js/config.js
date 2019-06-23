f45946e5c871ddbd851b987f9c085fbc26775603
梁亚宾 <liangtongzhuo@gmail.com>
2019-06-23 09:13:40 +0800

    leancloud 被封

--- js/config.js
+++ js/config.js
@@ -2,7 +2,13 @@ const APP_ID = '2MY9AH1hE38iVn6cfSMeVXW8-gzGzoHsz'
 const APP_KEY = 'rhmGmvC4cz4qohsQlpmP0KV0'
 window.AV.init({
     appId: APP_ID,
-    appKey: APP_KEY
+    appKey: APP_KEY,
+    serverURLs: {
+        push: 'https://avoscloud.com',
+        stats: 'https://avoscloud.com',
+        engine: 'https://avoscloud.com',
+        api: 'https://avoscloud.com',
+    },
 });
 //标签
 const tagStr = '首页,GitHub,Node.js,Web,iOS,ReactNative,Swift,MongoDB,Vue,HTML,JavaScript,日常踩坑,动漫,工具,编程疑问,游戏,翻译,算法,书,非技术文章,我'
