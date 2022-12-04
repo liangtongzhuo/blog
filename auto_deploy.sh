1eb46da900a6dfe69ff88584b429a22afd71f58e
梁同桌 <liangyabin@datafountain.cn>
2022-12-04 11:55:14 +0100

    自动部署

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# 目录下执行下列语句，后台启动 shell，日志输出到 auto_deploy.log
+# 启动自动部署，当前目录执行下列语句。当前 shell 后台常驻运行，每30秒拉取一次git代码。 
 # nohup ./auto_deploy.sh > auto_deploy.log 2>&1 &
-# ps auxf | grep 'auto_deploy' 查看后台运行的脚步的 id，
+# 后台启动 shell，日志输出到 auto_deploy.log
+
+# 终止自动部署
+# ps auxf | grep 'auto_deploy' 查看后台运行的shell的 id，
 # kill id 杀死脚本
 
 while true
